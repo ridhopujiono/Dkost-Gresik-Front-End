60750fe761d7035bba37c3d8c4229c7853e55259
Ridho Pujiono <ridho@smktjp.sch.id>
2023-09-28 23:54:34 +0700

    Menambah fitur update read status notifikasi

--- app/Livewire/ResidentNotification.php
+++ app/Livewire/ResidentNotification.php
@@ -10,12 +10,18 @@ class ResidentNotification extends Component
 {
     public $notifs;
 
+    public function changeReadStatus()
+    {
+        Http::post(env('APP_URL_API') . "/notification/update_read_status/user_id/" . auth()->user()->id)->json()['data'];
+    }
+
     public function render()
     {
         try {
             $this->notifs = Http::post(env('APP_URL_API') . "/notification/user_id/" . auth()->user()->id)->json()['data'];
+            $this->changeReadStatus();
         } catch (Exception $e) {
-            dd($e);
+            session()->flash('error', 'Ada kesalahan sistem. Error' . $e->getMessage());
         }
         return view('livewire.resident-notification');
     }
